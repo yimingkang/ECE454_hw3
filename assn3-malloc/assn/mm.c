fc6088aefe7511873a8e3fab14efee1ded5bb5df
yimingkang <yimingkang@hotmail.com>
2015-10-22 14:43:40 -0400

    still working on it

--- assn3-malloc/assn/mm.c
+++ assn3-malloc/assn/mm.c
@@ -24,15 +24,15 @@
  ********************************************************/
 team_t team = {
     /* Team name */
-    "",
+    "Smash bros",
     /* First member's full name */
-    "",
+    "Yiming Kang",
     /* First member's email address */
-    "",
+    "yiming.kang@mail.utoronto.ca",
     /* Second member's full name (leave blank if none) */
-    "",
+    "Zexuan Wang",
     /* Second member's email address (leave blank if none) */
-    ""
+    "" // TODO
 };
 
 /*************************************************************************
@@ -57,12 +57,22 @@ team_t team = {
 #define GET_ALLOC(p)    (GET(p) & 0x1)
 
 /* Given block ptr bp, compute address of its header and footer */
-#define HDRP(bp)        ((char *)(bp) - WSIZE)
-#define FTRP(bp)        ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+#define HDRP(bp)        ((char *)(bp))
+#define FTRP(bp)        ((char *)(bp) + GET_SIZE(HDRP(bp)) - WSIZE)
 
 /* Given block ptr bp, compute address of next and previous blocks */
-#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
-#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+#define NEXT_BLKP(bp)   (*((char *)(bp) + WSIZE))
+#define PREV_BLKP(bp)   (*((char *)(bp) + DSIZE))
+
+/* For coalesce */
+#define PHYSICAL_NEXT_W(bp) ((char *)(bp) + GET_SIZE((char *)(bp)))
+#define PHYSICAL_PREV_W(bp) ((char *)(bp) - WSIZE)
+
+/* Set next/previous pointer*/
+#define SET_NEXT(f, t)  PUT(f + WSIZE, t)
+#define SET_PREV(f, t)  PUT(f + DSIZE, t)
+
+#define IS_END(bp)      (GET_SIZE(bp) == 0)
 
 void* heap_listp = NULL;
 
@@ -73,14 +83,13 @@ void* heap_listp = NULL;
  **********************************************************/
  int mm_init(void)
  {
-   if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
+     if ((heap_listp = mem_sbrk(3*WSIZE)) == (void *)-1)
          return -1;
-     PUT(heap_listp, 0);                         // alignment padding
-     PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));   // prologue header
-     PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));   // prologue footer
-     PUT(heap_listp + (3 * WSIZE), PACK(0, 1));    // epilogue header
-     heap_listp += DSIZE;
-
+     PUT(heap_listp, 0);                 // alignment padding
+     PUT(heap_listp + (1 * WSIZE), 0);   // Heap pointer
+     PUT(heap_listp + (1 * WSIZE), PACK(0, 1));   // Begining of heap indicator
+     PUT(heap_listp + (1 * WSIZE), PACK(0, 1));   // End of heap indicator
+     heap_listp += WSIZE;
      return 0;
  }
 
@@ -94,12 +103,13 @@ void* heap_listp = NULL;
  **********************************************************/
 void *coalesce(void *bp)
 {
-    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
-    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    /* Find physical prev and next block */
+    size_t prev_alloc = GET_ALLOC(PHYSICAL_PREV_W(bp));
+    size_t next_alloc = GET_ALLOC(PHYSICAL_NEXT_W(bp));
     size_t size = GET_SIZE(HDRP(bp));
 
     if (prev_alloc && next_alloc) {       /* Case 1 */
-        return bp;
+        return insert_block(bp);
     }
 
     else if (prev_alloc && !next_alloc) { /* Case 2 */
@@ -133,6 +143,7 @@ void *coalesce(void *bp)
  **********************************************************/
 void *extend_heap(size_t words)
 {
+    // basically dont have to worry about this function
     char *bp;
     size_t size;
 
@@ -141,13 +152,33 @@ void *extend_heap(size_t words)
     if ( (bp = mem_sbrk(size)) == (void *)-1 )
         return NULL;
 
+    /* shift this block up by one word */
+    bp -= WSIZE;
+
     /* Initialize free block header/footer and the epilogue header */
     PUT(HDRP(bp), PACK(size, 0));                // free block header
     PUT(FTRP(bp), PACK(size, 0));                // free block footer
-    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));        // new epilogue header
+    PUT(FTRP(bp) + WSIZE, PACK(0, 1));           // end of heap indicator
 
-    /* Coalesce if the previous block was free */
     return coalesce(bp);
+}
+
+void *insert_block(char *bp){
+    // 1- Get the next element from head
+    void *tmp = NEXT_BLKP(heap_listp);
+
+    // 2- prev of bp is heap_listp, next of heap_listp is bp
+    SET_PREV(bp, heap_listp);
+    SET_NEXT(heap_listp, bp)
+
+    // 3- if not the end, tmp.prev = bp
+    if (!IS_END(tmp)){
+        SET_PREV(tmp, bp)
+    }
+    
+    // 4- bp.next = tmp
+    SET_NEXT(bp, tmp)
+    return bp
 }
 
 
@@ -160,6 +191,7 @@ void *extend_heap(size_t words)
 void * find_fit(size_t asize)
 {
     void *bp;
+    // To find fit, search from heap_listp (which is the 'root')
     for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp))
     {
         if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp))))
@@ -218,10 +250,14 @@ void *mm_malloc(size_t size)
         return NULL;
 
     /* Adjust block size to include overhead and alignment reqs. */
+    
     if (size <= DSIZE)
         asize = 2 * DSIZE;
     else
         asize = DSIZE * ((size + (DSIZE) + (DSIZE-1))/ DSIZE);
+
+    // Extra 2 words of overhead from explicit list (2 pointers)
+    size += DSIZE;
 
     /* Search the free list for a fit */
     if ((bp = find_fit(asize)) != NULL) {
