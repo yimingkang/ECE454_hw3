11771a83b2f587e26d0a3e61399eed03b64f44e3
yimingkang <yimingkang@hotmail.com>
2015-10-22 21:27:44 -0400

    still working on it

--- assn3-malloc/assn/mm.c
+++ assn3-malloc/assn/mm.c
@@ -18,6 +18,8 @@
 #include "mm.h"
 #include "memlib.h"
 
+#define DEBUG
+
 /*********************************************************
  * NOTE TO STUDENTS: Before you do anything else, please
  * provide your team information in the following struct.
@@ -32,7 +34,7 @@ team_t team = {
     /* Second member's full name (leave blank if none) */
     "Zexuan Wang",
     /* Second member's email address (leave blank if none) */
-    "" // TODO
+    "TODO@TODO.TODO" // TODO
 };
 
 /*************************************************************************
@@ -50,7 +52,7 @@ team_t team = {
 
 /* Read and write a word at address p */
 #define GET(p)          (*(uintptr_t *)(p))
-#define PUT(p,val)      (*(uintptr_t *)(p) = (val))
+//#define PUT(p,val)      (*(uintptr_t *)(p) = (val))
 
 /* Read the size and allocated fields from address p */
 #define GET_SIZE(p)     (GET(p) & ~(DSIZE - 1))
@@ -71,10 +73,10 @@ team_t team = {
 #define PHYSICAL_NEXT_BLK(bp) PHYSICAL_NEXT_W(bp)
 
 /* Set next/previous pointer*/
-#define SET_NEXT(f, t)  PUT(f + WSIZE, t)
-#define SET_PREV(f, t)  PUT(f + DSIZE, t)
+#define SET_NEXT(f, t)  PUT((char *)f + WSIZE, (uint64_t) t)
+#define SET_PREV(f, t)  PUT((char *)f + DSIZE, (uint64_t) t)
 
-#define IS_END(bp)      (GET_SIZE(bp) == 0)
+#define IS_END(bp)      GET(bp) == 0 ? 1 : 0
 
 void* heap_listp = NULL;
 
@@ -89,17 +91,24 @@ void blk_unlink(void *);
  **********************************************************/
  int mm_init(void)
  {
-     if ((heap_listp = mem_sbrk(3*WSIZE)) == (void *)-1)
+     puts("mm_init is called");
+     if ((heap_listp = mem_sbrk(6*WSIZE)) == (void *)-1)
          return -1;
-     PUT(heap_listp + (1 * WSIZE), PACK(2 * DSIZE, 1));   // Header
+     PUT(heap_listp, PACK(2 * DSIZE, 1));   // Header
      PUT(heap_listp + (1 * WSIZE), 0);                    // Next pointer
-     PUT(heap_listp + (1 * WSIZE), 0);                    // Prev pointer
-     PUT(heap_listp + (1 * WSIZE), PACK(2 * DSIZE, 1));   // Footer
-     PUT(heap_listp + (1 * WSIZE), PACK(0, 1));           // Begining of heap indicator
-     PUT(heap_listp + (1 * WSIZE), PACK(0, 1));           // End of heap indicator
+     PUT(heap_listp + (2 * WSIZE), 0);                    // Prev pointer
+     PUT(heap_listp + (3 * WSIZE), PACK(2 * DSIZE, 1));   // Footer
+     PUT(heap_listp + (4 * WSIZE), PACK(0, 1));           // Begining of heap indicator
+     PUT(heap_listp + (5 * WSIZE), PACK(0, 1));           // End of heap indicator
+     printf("mm_init heap is at 0x%x, heap brk is at 0x%x\n", heap_listp, heap_listp + 5 * WSIZE);
+     puts("mm_init is done");
      return 0;
  }
 
+void PUT(uintptr_t p, uint64_t val){
+    printf("Writing 0x%x into mem, WSIZE is %d\n", val, WSIZE);
+    memcpy(p, &val, WSIZE);
+}
 
 
 /**********************************************************
@@ -112,16 +121,21 @@ void blk_unlink(void *);
  **********************************************************/
 void *coalesce(void *bp)
 {
+    printf("coalesce is called with 0x%x\n", bp);
     /* Find physical prev and next block */
     size_t prev_alloc = GET_ALLOC(PHYSICAL_PREV_W(bp));
     size_t next_alloc = GET_ALLOC(PHYSICAL_NEXT_W(bp));
     size_t size = GET_SIZE(HDRP(bp));
 
+    printf("coalesce prev_alloc: %d ... next_alloc: %d\n", prev_alloc, next_alloc);
+
     if (prev_alloc && next_alloc) {       /* Case 1 */
+        puts("coalesce case 1");
         return insert_block(bp);
     }
 
     else if (prev_alloc && !next_alloc) { /* Case 2 */
+        puts("coalesce case 2");
         // Size of next block is stored in the next word
         size += GET_SIZE(PHYSICAL_NEXT_W(bp));
         void *next_blk = PHYSICAL_NEXT_BLK(bp);
@@ -136,6 +150,7 @@ void *coalesce(void *bp)
     }
 
     else if (!prev_alloc && next_alloc) { /* Case 3 */
+        puts("coalesce case 3");
         // Size of prev. block is sotred in the word
         // immediately before bp's header
         size += GET_SIZE(PHYSICAL_PREV_W(bp));
@@ -153,6 +168,7 @@ void *coalesce(void *bp)
     }
 
     else {            /* Case 4 */
+        puts("coalesce case 4");
         size += GET_SIZE(PHYSICAL_PREV_W(bp));
         size += GET_SIZE(PHYSICAL_NEXT_W(bp));
 
@@ -179,11 +195,15 @@ void *extend_heap(size_t words)
     // basically dont have to worry about this function
     char *bp;
     size_t size;
+    puts("extend_heap is called");
 
     /* Allocate an even number of words to maintain alignments */
     size = (words % 2) ? (words+1) * WSIZE : words * WSIZE;
-    if ( (bp = mem_sbrk(size)) == (void *)-1 )
+
+    printf("extend_heap is going to call mem_sbrk with %d\n", size);
+    if ( (bp = mem_sbrk(size)) == (void *)-1 && puts("SBRK ERROR!"))
         return NULL;
+    printf("extend_heap old end is: 0x%x\n", bp - WSIZE);
 
     /* shift this block up by one word */
     bp -= WSIZE;
@@ -193,6 +213,7 @@ void *extend_heap(size_t words)
     PUT(FTRP(bp), PACK(size, 0));                // free block footer
     PUT(FTRP(bp) + WSIZE, PACK(0, 1));           // end of heap indicator
 
+    //set bp to header of block
     return coalesce(bp);
 }
 
@@ -206,15 +227,21 @@ void *extend_heap(size_t words)
 void * find_fit(size_t asize)
 {
     void *bp;
+    printf("find_fit is called with %d\n", asize);
     // To find fit, search from heap_listp (which is the 'root')
     // bp = 0 indicates end of heap
+    printf("HEAP NEXT IS %p\n", *((char *)heap_listp + WSIZE));
+    printf("HEAP PREV IS %p\n", *((char *)heap_listp + DSIZE));
     for (bp = heap_listp; bp && GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp))
     {
+        printf("find_fit is checking block at 0x%x\n", bp);
         if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp))))
         {
+            printf("find_fit found a suitable block at 0x%x\n", bp);
             return bp;
         }
     }
+    puts("find_fit cannot find a good block, done");
     return NULL;
 }
 
@@ -224,8 +251,10 @@ void * find_fit(size_t asize)
  **********************************************************/
 void place(void* bp, size_t asize)
 {
+
   /* Get the current block size */
   size_t bsize = GET_SIZE(HDRP(bp));
+  printf("place placing at 0x%x, real_size %d, adjusted_size %d\n", bp, asize, bsize);
 
   PUT(HDRP(bp), PACK(bsize, 1));
   PUT(FTRP(bp), PACK(bsize, 1));
@@ -237,6 +266,7 @@ void place(void* bp, size_t asize)
  **********************************************************/
 void mm_free(void *bp)
 {
+    puts("mm_free is called");
     if(bp == NULL){
       return;
     }
@@ -244,6 +274,7 @@ void mm_free(void *bp)
     // Header info is in the word before bp
     bp -= WSIZE;
     size_t size = GET_SIZE(HDRP(bp));
+    printf("mm_free freeing block starting at 0x%x, size %d\n", bp, size);
 
     // Restore allocation bit to 0
     PUT(HDRP(bp), PACK(size,0));
@@ -276,6 +307,9 @@ void *mm_malloc(size_t size)
     else
         asize = DSIZE * ((size + (DSIZE) + (DSIZE-1))/ DSIZE);
 
+    printf("mm_malloc is called with size %d, adjusted to %d\n", size, asize);
+    printf("mm_malloc current heap pointer is at 0x%x\n", heap_listp);
+
     /* Search the free list for a fit */
     if ((bp = find_fit(asize)) != NULL) {
         // first unlink bp from its neighbors 
@@ -290,8 +324,11 @@ void *mm_malloc(size_t size)
 
     /* No fit found. Get more memory and place the block */
     extendsize = MAX(asize, CHUNKSIZE);
-    if ((bp = extend_heap(extendsize/WSIZE)) == NULL)
+    bp = extend_heap(extendsize/WSIZE);
+    printf("mm_malloc extend_heap returned 0x%x\n", bp);
+    if (bp == NULL)
         return NULL;
+    printf("mm_malloc placing bp\n");
     place(bp, asize);
 
     /* MUST NOT include the header */
@@ -342,20 +379,29 @@ int mm_check(void){
 }
 
 void *insert_block(void *bp){
+    printf("insert_block: block to be inserted 0x%x\n", bp);
+    printf("insert_block: head of heap 0x%x\n", heap_listp);
+    printf("insert_block: heap->next = 0x%x\n", *((char *)heap_listp + WSIZE));
     // 1- Get the next element from head
-    void *tmp = NEXT_BLKP(heap_listp);
+    void *next_ptr = NEXT_BLKP(heap_listp);
 
     // 2- prev of bp is heap_listp, next of heap_listp is bp
+    printf("insert_block setting bp->prev = 0x%x\n", heap_listp);
     SET_PREV(bp, heap_listp);
-    SET_NEXT(heap_listp, bp);
 
-    // 3- if not the end, tmp.prev = bp
-    if (!IS_END(tmp)){
-        SET_PREV(tmp, bp);
+    printf("insert_block setting heap->next = 0x%x\n", bp);
+    SET_NEXT(heap_listp, bp);
+    printf("HEAP NEXT IS 0x%x\n", (uint64_t) *((char *)heap_listp + WSIZE));
+
+    if (next_ptr){
+        puts("insert_block heap->next is not NULL");
+        SET_PREV(next_ptr, bp);
     }
-    
+
     // 4- bp.next = tmp
-    SET_NEXT(bp, tmp);
+    printf("insert_block setting bp->next = 0x%x\n", next_ptr);
+    SET_NEXT(bp, next_ptr);
+    puts("insert_block done");
     return bp;
 }
 
